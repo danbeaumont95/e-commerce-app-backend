7389b461644b9df7064363e66827994cb767e14f
Dan Beaumont <danielbeaumont95@hotmail.co.uk>
2022-04-26 21:59:54 +0100

    added new packages and created better ways to get/post item

--- main.py
+++ main.py
@@ -1,51 +1,77 @@
-from pydantic import BaseModel
-from fastapi import FastAPI
+
+import os
+from fastapi import FastAPI, Body, HTTPException, status
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel, Field, EmailStr
+from bson import ObjectId
+from typing import Optional, List
+import motor.motor_asyncio
 from dotenv import load_dotenv, dotenv_values
-import pymongo
-import certifi
-app = FastAPI()
-
-
-class Item(BaseModel):
-    name: str
-    description: str = None
-    price: int
-    seller: str = None
-
-# uvicorn main:app --reload
-
 
 config = dotenv_values(".env")
 db_name = config['db_name']
 db_username = config['db_username']
 db_password = config['db_password']
 
-
+# uvicorn main:app --reload
 load_dotenv()
-conn = pymongo.MongoClient(
-    f'mongodb+srv://{db_username}:{db_password}@e-commerce-app.z4k96.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', tlsCAFile=certifi.where())
-db = conn[f'{db_name}']
+
 app = FastAPI()
-papersCol = db['e-commerce']
-items = db['items']
+client = motor.motor_asyncio.AsyncIOMotorClient(
+    f"mongodb+srv://{db_username}:{db_password}@e-commerce-app.z4k96.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", tls=True, tlsAllowInvalidCertificates=True)
+db = client['e-commerce-app']
+print(db, 'dbdbdb')
 
 
-@app.get("/items")
-def root():
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
 
-    alItems = list(items.find())
-    print(alItems, 'all')
-    print('hi')
-    return {"Items": f"{alItems}"}
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
 
 
-@app.post('/item')
-def create_course(item: Item):
+class ItemModel(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    seller: str = Field(...)
+    description: str = Field(...)
+    name: str = Field(...)
+    price: int = Field(...)
 
-    new_item = {"name": f"{item.name}", "description": f"{item.description}",
-                "price": f"{item.price}", "seller": f"{item.seller}"}
-    print(new_item, 'new_item')
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "seller": "Jane Doe",
+                "description": "test description",
+                "name": "Dan",
+                "price": "3.0",
+            }
+        }
 
-    x = items.insert_one(new_item)
 
-    return item
+@app.post("/item", response_description="Add new item", response_model=ItemModel)
+async def create_item(item: ItemModel = Body(...)):
+    item = jsonable_encoder(item)
+    new_item = await db["items"].insert_one(item)
+    created_item = await db["items"].find_one({"_id": new_item.inserted_id})
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_item)
+
+
+@app.get(
+    "/", response_description="List all items", response_model=List[ItemModel]
+)
+async def list_items():
+    items = await db["items"].find().to_list(1000)
+    return items
