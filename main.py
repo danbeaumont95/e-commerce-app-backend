12fbac9043efdb24ad9177f580b806633102b0b8
Dan Beaumont <danielbeaumont95@hotmail.co.uk>
2022-04-27 20:26:54 +0100

    created UserModel and post user route

--- main.py
+++ main.py
@@ -39,6 +39,31 @@ class PyObjectId(ObjectId):
         field_schema.update(type="string")
 
 
+class UserModel(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    firstName: str = Field(...)
+    lastName: str = Field(...)
+    email: str = Field(...)
+    password: str = Field(...)
+    mobileNumber: int = Field(...)
+    username: str = Field(...)
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "firstName": "Jane",
+                "lastName": "Doe",
+                "email": "test@email.com",
+                "password": "password",
+                "username": "janedoe",
+                "mobileNumber": 447515538351,
+            }
+        }
+
+
 class ItemModel(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
     seller: str = Field(...)
@@ -86,14 +111,14 @@ async def create_item(item: ItemModel = Body(...)):
 
 
 @app.get(
-    "/", response_description="List all items", response_model=List[ItemModel]
+    "/item", response_description="List all items", response_model=List[ItemModel]
 )
 async def list_items():
     items = await db["items"].find().to_list(1000)
     return items
 
 
-@app.put("/{id}", response_description="Updates an item", response_model=ItemModel)
+@app.put("/item/{id}", response_description="Updates an item", response_model=ItemModel)
 async def update_item(id: str, item: UpdateItemModel = Body(...)):
     item = {k: v for k, v in item.dict().items() if v is not None}
     if len(item) >= 1:
@@ -108,16 +133,24 @@ async def update_item(id: str, item: UpdateItemModel = Body(...)):
     raise HTTPException(status_code=404, detail=f"Item {id} not found")
 
 
-@app.get("/{id}", response_description="Gets an item by id", response_model=ItemModel)
+@app.get("/item/{id}", response_description="Gets an item by id", response_model=ItemModel)
 async def get_item(id: str):
     if (item := await db['items'].find_one({"_id": id})) is not None:
         return item
     raise HTTPException(status_code=404, detail=f"Item {id} not found")
 
 
-@app.delete("/{id}", response_description="Deletes an item")
+@app.delete("/item/{id}", response_description="Deletes an item")
 async def delete_item(id: str):
     delete_result = await db['items'].delete_one({"_id": id})
     if delete_result.deleted_count == 1:
         return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
     raise HTTPException(status_code=404, detail=f"Item {id} not found")
+
+
+@app.post('/user')
+async def create_user(user: UserModel = Body(...)):
+    user = jsonable_encoder(user)
+    new_user = await db['users'].insert_one(user)
+    created_user = await db['users'].find_one({"_id": new_user.inserted_id})
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_user)
