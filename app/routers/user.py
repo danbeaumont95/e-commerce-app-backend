fdc6e4c85e02d2ee24649a2c502ee9d896b903eb
Dan Beaumont <danielbeaumont95@hotmail.co.uk>
2022-04-30 12:56:17 +0100

    login route now returns access and refresh token and saves in db

--- app/routers/user.py
+++ app/routers/user.py
@@ -27,19 +27,27 @@ def decodeJWT(token: str) -> dict:
         return {}
 
 
-def token_response(token: str):
+def token_response(access_token: str, refresh_token: str):
     return {
-        "access_token": token
+        "access_token": access_token,
+        "refresh_token": refresh_token
     }
 
 
 def signJWT(user_id: str) -> Dict[str, str]:
-    payload = {
+    access_payload = {
         "user_id": user_id,
         "expires": time.time() + 600
     }
-    token = jwt.encode(payload, jwt_secret, algorithm=jwt_algorithm)
-    return token_response(token)
+    refresh_payload = {
+        "user_id": user_id,
+        "expires": time.time() + 30000000
+    }
+    access_token = jwt.encode(
+        access_payload, jwt_secret, algorithm=jwt_algorithm)
+    refresh_token = jwt.encode(
+        refresh_payload, jwt_secret, algorithm=jwt_algorithm)
+    return token_response(access_token, refresh_token)
 
 
 @router.post('/', tags=['user'])
