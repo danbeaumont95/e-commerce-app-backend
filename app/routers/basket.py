97d48757f3f8801019d5d5a601c0078c8d95a8ef
Dan Beaumont <danielbeaumont95@hotmail.co.uk>
2022-05-01 22:36:30 +0100

    added add item to basket api

--- app/routers/basket.py
+++ app/routers/basket.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Body, status
+from fastapi import APIRouter, Body, status, Request
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
-from ..basket.model import BasketModel
+from ..basket.model import BasketModel, ItemToAdd
 from ..db import db
+from .item import decodeJWT
 
 router = APIRouter(
     prefix="/basket",
@@ -22,3 +23,28 @@ async def create_basket(basket: BasketModel = Body(...)):
     created_basket = await db['users'].find_one({"_id": new_basket.inserted_id})
 
     return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_basket)
+
+
+@router.post('/addToBasket')
+async def add_item_to_basket(request: Request, item: ItemToAdd = Body(...)):
+
+    bearer_token = request.headers.get('authorization')
+
+    access_token = bearer_token[7:]
+    isAllowed = decodeJWT(access_token)
+
+    if isAllowed is not None:
+
+        user_id = isAllowed['user_id']
+        item = jsonable_encoder(item)
+        await db['baskets'].update_one({"userId": user_id}, {"$push": {
+            "items": item
+        }})
+
+        return {
+            "Message": "Item added to basket!"
+        }
+    else:
+        return {
+            "Message": "Token expired, please log in again"
+        }
