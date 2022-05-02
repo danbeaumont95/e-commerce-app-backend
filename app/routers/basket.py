1643acde67e8d27ed36454f510c6b52f6fb3d9a2
Dan Beaumont <danielbeaumont95@hotmail.co.uk>
2022-05-02 10:51:27 +0100

    added delete item from basket api

--- app/routers/basket.py
+++ app/routers/basket.py
@@ -68,3 +68,33 @@ async def get_my_basket(request: Request):
         return {
             "error": "Token expired, please log in again"
         }
+
+
+@router.delete('/{id}', tags=['basket'], response_description='delets an item from basket')
+async def delete_item_from_basket(request: Request, id: str):
+    bearer_token = request.headers.get('authorization')
+
+    access_token = bearer_token[7:]
+    isAllowed = decodeJWT(access_token)
+    if isAllowed is not None:
+        user_id = isAllowed['user_id']
+        user_basket = await db['baskets'].find_one({"userId": user_id})
+
+        try:
+            await db['baskets'].update_one({"_id": user_basket['_id']}, {"$pull": {
+                "items": {"itemId": id}
+            }})
+
+            updated_user_basket = await db['baskets'].find_one({"userId": user_id})
+
+            if len(updated_user_basket['items']) < len(user_basket['items']):
+                return {"message": "Item deleted"}
+            else:
+                return {"error": "Unable to delete item"}
+
+        except:
+            return {"error": "Unable to delete item"}
+    else:
+        return {
+            "error": "Token expired, please log in again"
+        }
