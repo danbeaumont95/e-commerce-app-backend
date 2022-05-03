ec4754cffd9397b1428f75c453860c817ad644fc
Dan Beaumont <danielbeaumont95@hotmail.co.uk>
2022-05-03 19:01:45 +0100

    changed item model so when user creates item they add an image at same time

--- app/db.py
+++ app/db.py
@@ -7,7 +7,8 @@ db_username = os.getenv("db_username")
 db_password = os.getenv("db_password")
 jwt_secret = os.getenv('secret')
 jwt_algorithm = os.getenv('algorithm')
-
+aws_access_key_id = os.getenv('aws_access_key_id')
+aws_secret_access_key = os.getenv('aws_secret_access_key')
 
 client = motor.motor_asyncio.AsyncIOMotorClient(
     f"mongodb+srv://{db_username}:{db_password}@e-commerce-app.z4k96.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", tls=True, tlsAllowInvalidCertificates=True)
